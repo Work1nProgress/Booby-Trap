463d6011b1f5f7bd77f8298eadf5f2835696991c
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-05-23 00:00:06 +0200

    more spear fixes

--- Assets/Scripts/Runtime/Player/FlyingSpear.cs
+++ Assets/Scripts/Runtime/Player/FlyingSpear.cs
@@ -20,13 +20,22 @@ public class FlyingSpear : Spear
     [SerializeField]
     Rigidbody2D m_RigidBody;
 
-    Vector3 stuckOffset = new Vector3(0.23225f, 0, 0);
+
 
 
 
     public void Init(float speed, float lifeTime, float range, float echoSpeed, int direction, float inheritSpeed, UnityAction OnDespawnedCallback)
     {
-        m_RigidBody.velocity = new Vector2(speed* direction + echoSpeed * inheritSpeed, 0);
+        if (direction == 0)
+        {
+            m_RigidBody.velocity = new Vector2(0,speed + echoSpeed * inheritSpeed);
+
+        }
+        else
+        {
+            m_RigidBody.velocity = new Vector2(speed * direction + echoSpeed * inheritSpeed, 0);
+        }
+
         m_Range = range;
         m_StartPositon = transform.position;
         Init(lifeTime, direction, OnDespawnedCallback);
@@ -53,7 +62,7 @@ public class FlyingSpear : Spear
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        var spear = PoolManager.Spawn<StuckSpear>("StuckSpear", null, transform.position + m_Direction * stuckOffset, Quaternion.Euler(0, 0, 90));
+        var spear = PoolManager.Spawn<StuckSpear>("StuckSpear", null, transform.position + m_Direction * StuckOffset, Quaternion.Euler(0, 0, 90));
         spear.Init(m_Lifetime,
            m_Direction,
             m_OnDespawnedCallback);
--- Assets/Scripts/Runtime/Player/Player.cs
+++ Assets/Scripts/Runtime/Player/Player.cs
@@ -54,6 +54,9 @@ public class Player : EntityBase
     int m_CurrentSpearAmount;
 
 
+    float inputY;
+
+
     bool CanThrowSpear => (m_CurrentSpearAmount > 0 || m_MaxSpearsOnPlayer < 0) && (m_ThrowTimer < 0 || m_ThrowCooldown == 0);
 
 
@@ -62,11 +65,14 @@ public class Player : EntityBase
     {
         ControllerInput.Instance.Attack.AddListener(OnThrow);
         ControllerInput.Instance.Throw.AddListener(OnAttack);
+        ControllerInput.Instance.Vertical.AddListener(OnVertical);
     }
 
     private void OnDisable()
     {
-        
+        ControllerInput.Instance.Attack.RemoveListener(OnThrow);
+        ControllerInput.Instance.Throw.RemoveListener(OnAttack);
+        ControllerInput.Instance.Vertical.RemoveListener(OnVertical);
     }
 
     private void Start()
@@ -93,15 +99,39 @@ public class Player : EntityBase
     {
         if (CanThrowSpear)
         {
+            // will change this when we add the throwing the spear up and down
+             float speed = m_MovementController.RigidBody.velocity.x;
+             int direction = m_MovementController.FacingRight ? 1 : -1;
+
+            var hit = Physics2D.Raycast(transform.position, direction * Vector2.right, 0.5f, LayerMask.GetMask("Ground"));
+
+
             m_ThrowTimer = m_ThrowCooldown;
-            var spear = PoolManager.Spawn<FlyingSpear>("FlyingSpear", null, transform.position, Quaternion.Euler(0, 0, 90));
-            spear.Init(m_SpearFlySpeed,
-                m_SpearLifetime,
-                m_SpearRange,
-                m_MovementController.RigidBody.velocity.x, //speed of echo
-                m_MovementController.FacingRight ? 1 : -1,  //direction
-                m_InheritSpeed,                            //amount of inherited speed   
-                ReturnSpear);
+            Spear spear;
+            if (hit)
+            {
+                var stuckSpear = PoolManager.Spawn<StuckSpear>("StuckSpear", null, new Vector3(hit.point.x, hit.point.y, 0) - direction * Spear.StuckOffset, Quaternion.Euler(0, 0, 90));
+                stuckSpear.Init(m_SpearLifetime,
+                   direction,  //direction                       
+                    ReturnSpear);
+                spear = stuckSpear;
+            }
+            else
+            {
+
+                var thrownSpear = PoolManager.Spawn<FlyingSpear>("FlyingSpear", null, transform.position + direction * new Vector3(0.5f, 0, 0), Quaternion.Euler(0, 0, 90));
+                thrownSpear.Init(m_SpearFlySpeed,
+                    m_SpearLifetime,
+                    m_SpearRange,
+                   speed, //speed of echo
+                   direction,  //direction
+                    m_InheritSpeed,//amount of inherited speed   
+                    ReturnSpear);
+
+                spear = thrownSpear;
+            }
+
+
 
 
             if (m_MaxNumberOfSpearsInGame > 0 && ControllerGame.Instance.Spears.Count + 1 >= m_MaxNumberOfSpearsInGame)
@@ -118,6 +148,11 @@ public class Player : EntityBase
     void OnAttack()
     {
 
+    }
+
+    void OnVertical(float value)
+    {
+        inputY = value;
     }
 
 
