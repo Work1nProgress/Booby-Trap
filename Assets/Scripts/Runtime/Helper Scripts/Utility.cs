1c9f9e50a825f03e0a156bbce6b6a102cda5f3f2
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-05-25 00:05:49 +0200

    v1 soundmanager

--- Assets/Scripts/Runtime/Helper Scripts/Utility.cs
+++ Assets/Scripts/Runtime/Helper Scripts/Utility.cs
@@ -122,5 +122,47 @@ public class CountdownTimer
             _timeLeft = newTime;
     }
 
-  
+
+   
+
+}
+
+public class Utils
+{
+    static public T AddArrayElement<T>(ref T[] array, T elToAdd)
+    {
+        if (array == null)
+        {
+            array = new T[1];
+            array[0] = elToAdd;
+            return elToAdd;
+        }
+
+        var newArray = new T[array.Length + 1];
+        array.CopyTo(newArray, 0);
+        newArray[array.Length] = elToAdd;
+        array = newArray;
+        return elToAdd;
+    }
+
+    static public void DeleteArrayElement<T>(ref T[] array, int index)
+    {
+        if (index >= array.Length || index < 0)
+        {
+            Debug.LogWarning("invalid index in DeleteArrayElement: " + index);
+            return;
+        }
+        var newArray = new T[array.Length - 1];
+        int i;
+        for (i = 0; i < index; i++)
+        {
+            newArray[i] = array[i];
+        }
+        for (i = index + 1; i < array.Length; i++)
+        {
+            newArray[i - 1] = array[i];
+        }
+        array = newArray;
+    }
+
 }
--- Assets/Scripts/Runtime/Manager Scripts/SoundManager.cs
+++ Assets/Scripts/Runtime/Manager Scripts/SoundManager.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : GenericSingleton<SoundManager>
 {
 
-    [SerializeField]
-    AudioMixerGroup Mixer;
+ 
+    public AudioMixerGroup Mixer;
 
     [SerializeField] private List<Sound> sounds;
 
@@ -20,10 +21,27 @@ public class SoundManager : GenericSingleton<SoundManager>
     List<SoundInstance> RecentSounds = new List<SoundInstance>();
     Dictionary<string, SoundItem> LastPlayed;
 
-    public float MinimumTimeBetweenSameSound;
+
     string SoundInstanceName = "SoundInstance";
 
+    public float MinDistance;
+    public float MaxDistance;
 
+    public AnimationCurve SpatialBlend = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 0));
+    public AnimationCurve Spread = new AnimationCurve(new Keyframe(0, 1), new Keyframe(360, 0));
+    public AnimationCurve CustomRolloff = new AnimationCurve(new Keyframe(0, 1), new Keyframe(0.2f,0.8f), new Keyframe(0.4f, 0.5f), new Keyframe(0.7f, 0.2f), new Keyframe(1, 0f));
+    public AnimationCurve ReverbZoneMix = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 0));
+
+    Dictionary<AudioSourceCurveType, AnimationCurve> curves = new Dictionary<AudioSourceCurveType, AnimationCurve>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+        curves.Add(AudioSourceCurveType.CustomRolloff, CustomRolloff);
+        curves.Add(AudioSourceCurveType.ReverbZoneMix, ReverbZoneMix);
+        curves.Add(AudioSourceCurveType.Spread, Spread);
+        curves.Add(AudioSourceCurveType.SpatialBlend, SpatialBlend);
+    }
 
     public void Play(string name, Transform target = default)
     {
@@ -38,7 +56,7 @@ public class SoundManager : GenericSingleton<SoundManager>
         {
             if (RecentSounds.FindIndex(x => x.SoundName == name) != -1)
             {
-                //Debug.Log($"already playing that sound");
+                Debug.Log($"already playing {name}");
                 return;
             }
             Vector3 targetPosition = target == default ? Camera.main.transform.position : target.position;
@@ -48,19 +66,22 @@ public class SoundManager : GenericSingleton<SoundManager>
             {
                 LastPlayed.TryGetValue(name, out previous);
             }
+
+            SceneManager.MoveGameObjectToScene(soundInstance.gameObject, SceneManager.GetActiveScene());
             var next = sounds[idx].SoundItem(previous);
-            soundInstance.Set(next, target);
+            soundInstance.name = $"{sounds[idx].SoundName}_{idx}";
+            soundInstance.Set(next, sounds[idx], target, curves, MinDistance, MaxDistance);
             var length = soundInstance.Play();
 
             PlayingSounds.Add(soundInstance);
-            if (MinimumTimeBetweenSameSound > 0)
+            if (sounds[idx].MinimumTimeBetweenSameSound > 0)
             {
                 RecentSounds.Add(soundInstance);
+                StartCoroutine(RemoveFromList(Mathf.Min(sounds[idx].MinimumTimeBetweenSameSound, soundInstance.Length), RecentSounds, soundInstance));
             }
 
-
-
-
+            StartCoroutine(DespawnAfter(soundInstance.Length, PlayingSounds, soundInstance));
+            soundInstance.Play();
 
             if (sounds[idx].PlaySubitemType == PlaySubitemType.RandomNotSameTwice)
             {
