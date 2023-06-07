83543ca02421592e3f5cec1be821f56540e98ed8
CrashTheComet <82902878+CrashTheComet@users.noreply.github.com>
2023-06-07 11:53:52 -0500

    ui stuff

---
+++ Assets/Scripts/Runtime/UIScripts/MainMenu/MM_PrimaryMenuHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MM_PrimaryMenuHandler : GenericMenuHandler
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Pull the lever, Kronk!");
+    }
+
+    public void OtherThing()
+    {
+        Debug.Log("Oh shit! Double rainbow!!! (not really)");
+    }
+}
